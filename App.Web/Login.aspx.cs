ba98f80b1c67d8852144ec43e864df872c5018c1
Benjamin La Madrid <benjamin.lamadrid@factorit.com>
2018-08-20 21:48:04 -0300

    App base, con login no funcional

---
+++ App.Web/Login.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using App.Negocio;
+
+namespace App.Web
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        private Usuario sesionActiva
+        {
+            get
+            {
+                if (Session["miSesion"] == null)
+                {
+                    Session["miSesion"] = new Usuario();
+                }
+                return (Usuario) Session["miSesion"];
+            }
+            set
+            {
+                sesionActiva = value;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void BtnEntrar_Click(object sender, EventArgs e)
+        {
+            Usuario sesion = new Usuario();
+
+            sesion.Nombre = TxtUsuario.Text;
+            sesion.Clave = TxtClave.Text;
+
+            sesionActiva = sesion;
+
+            Server.TransferRequest("Home.aspx");
+        }
+    }
+}
